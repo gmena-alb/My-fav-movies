c17c3690d40256ce729ddc1261e3ccaf01111368
gr-mena <gmenadam@gmail.com>
2022-02-01 12:35:24 +0100

    pascal naming component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const query = graphql`
   {
@@ -11,19 +11,19 @@ const query = graphql`
       }
     }
   }
-`
+`;
 
-const SEO = ({ title, description }) => {
-  const { site } = useStaticQuery(query)
-  const metaDescription = description || site.siteMetadata.description
+const Seo = ({ title, description }) => {
+  const { site } = useStaticQuery(query);
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
-      htmlAttributes={{ lang: "en" }}
+      htmlAttributes={{ lang: 'en' }}
       title={`${title} - ${site.siteMetadata.title}`}
       meta={[{ name: `description`, content: metaDescription }]}
     ></Helmet>
-  )
-}
+  );
+};
 
-export default SEO
+export default Seo;
